c2ac15f7935b22f72df48b39919bfe377e584d3d
Keshwarsingh Nadan <5099324+keshwarsingh@users.noreply.github.com>
2026-05-20 14:39:04 +0400

    Update setup.sh

--- 2.73/bin/setup.sh
+++ 2.73/bin/setup.sh
@@ -1,34 +1,23 @@
 #!/bin/sh
 
-#BASE=/data/drivers/dbus-acload-prioritize
 BASE=$(dirname $(dirname $(realpath "$0")))
 
 echo "Set up device service to autorun on restart"
 
-chmod +x $BASE/dbus_acload_prioritize.py
+chmod +x "$BASE/dbus_acload_prioritize.py"
 
-# Use awk to inject correct BASE path into the run script
-awk -v base=$BASE '{gsub(/\$\{BASE\}/,base);}1' \
-    $BASE/bin/service/run.tmpl > $BASE/bin/service/run
+awk -v base="$BASE" '{gsub(/\$\{BASE\}/,base);}1' \
+    "$BASE/bin/service/run.tmpl" > "$BASE/bin/service/run"
 
-chmod -R a+rwx $BASE/bin/service
+chmod -R a+rwx "$BASE/bin/service"
 
 rm -f /service/dbus-acload-prioritize
+ln -s "$BASE/bin/service" /service/dbus-acload-prioritize &
+
+cat > /data/rc.local <<EOF
 ln -s $BASE/bin/service /service/dbus-acload-prioritize &
-
-CMD="ln -s $BASE/bin/service /service/dbus-acload-prioritize &"
-if ! grep -q "$CMD" /data/rc.local; then
-    echo "$CMD" >> /data/rc.local
-fi
-
-# -------------------------------
-# Enable IP forwarding
-# -------------------------------
-IPFWD_CMD="echo 1 > /proc/sys/net/ipv4/ip_forward"
-
-if ! grep -q "ip_forward" /data/rc.local; then
-    echo "$IPFWD_CMD" >> /data/rc.local
-fi
+echo 1 > /proc/sys/net/ipv4/ip_forward
+EOF
 
 chmod +x /data/rc.local
 
